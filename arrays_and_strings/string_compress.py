b0c00f1db0e21b780c2b2452095f960c1e2dca7b
Ruchir Khaitan <rk2660@columbia.edu>
2014-06-28 16:03:19 -0400

    tidied up simple string compression

--- arrays_and_strings/string_compress.py
+++ arrays_and_strings/string_compress.py
@@ -1,21 +1,37 @@
-string = "testsssstringggg"
-arr = list()
-for i in range (0, len(string)):
-    arr.append(0)
+#Computes easy string compression of the form
+#input sssssttrring
+#output s5t2r2i1n1g1
 
-character = string[0] 
-i = 0
-j = 0
-new_str = ""
-while j < len(string):
-    if string[j] == string[i]:
-        j = j +1
+#if the resultant "compression" takes more space than
+#the original, the original is returned
+
+def string_compress(string):
+    if isinstance(string, basestring) is not True:
+        print "ERROR: Please input a string"
+        return 
+         
+    i = 0
+    j = 0
+    new_str = ""
+    while j < len(string):
+        if string[j] == string[i]:
+            j = j +1
+        else:
+            new_str = new_str + string[i] + str(j - i)
+            i = j;
+            j = j + 1;
+
+    new_str = new_str + string[i] + str(j -i)
+
+    if len(new_str) > len(string):
+        return string
     else:
-        new_str = new_str + string[i] + str(j - i)
-        arr.insert(j, string[j])
-        i = j;
-        j = j + 1;
+        return new_str
 
-new_str = new_str + string[i] + str(j -i)
 
-print string, new_str
+def main():
+    string = "testsssssssssssssssssssssssssssstringggg"
+
+    print string, string_compress(string)
+
+main()
