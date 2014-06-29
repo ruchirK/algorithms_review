effa0a4d699dd0ad0b895304aa374705872f3c95
Ruchir Khaitan <rk2660@columbia.edu>
2014-06-29 19:50:19 -0400

    basic working atoi

---
+++ arrays_and_strings/atoi.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Code to reverse a string in place   
+
+//Test driver 
+int my_atoi(char* input ) {
+	if (input == NULL) {
+		return -1;
+	}
+	int i;
+	int value = 0;
+	int power = 1;
+	for(i = 0; i< strlen(input); i++){
+		if (input[i] >= '0' && input[i] <= '9'){
+			value = power * value  + (input[i] - '0');
+			power = 10;
+		}
+		else if (power > 1) {
+			break;
+		}
+	}
+	if (power > 1) { return value; }
+	return -1;
+}
+int main(int argc, char** argv) {
+	//Remember that char* and char[] are two different initializations and
+	//the first is immutable
+	char string_one[] = "he6438ffffe4567";
+	char string_two[] = "goodbye";
+	printf("Originals: %s %s\n", string_one, string_two);
+//	reverse(string_one);	
+//
+//	reverse(string_two);
+	int number = my_atoi(string_one);
+	printf("Reversed: %s %d\n", string_one, number);
+
+	return 0;
+
+}
+	
