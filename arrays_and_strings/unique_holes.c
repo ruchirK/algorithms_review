28e326345ac651ceee7fc275c40dc21142f29939
Ruchir Khaitan <rk2660@columbia.edu>
2014-06-30 23:48:36 +0000

    finalized disjoint sets in 2d matrix algorithm

--- arrays_and_strings/unique_holes.c
+++ arrays_and_strings/unique_holes.c
@@ -8,6 +8,9 @@ char check_point (char** array, int x, int y, int m, int n) {
         return (char) -1;
     } //invalid point 
 
+    if (array[x][y] >= '0') {
+        return array[x][y] - '0';
+    }
     return array[x][y];
 }
 
@@ -21,9 +24,7 @@ int check_neighbors(char* array[],int* set_num ,  int x, int y, int m, int n) {
     }
     //now we need to check the neighbors
     char left = check_point(array, x - 1, y, m, n);
-    //char right = check_point(array, x + 1, y, m, n);
-    char top = check_point(array, x, y + 1, m, n);
-    //char bottom = check_point(array, x, y - 1, m, n);
+    char top = check_point(array, x, y - 1, m, n);
     if (left > 1 ){
        array[x][y] = left;
        return 0;
@@ -32,9 +33,34 @@ int check_neighbors(char* array[],int* set_num ,  int x, int y, int m, int n) {
         array[x][y] = top;
         return 0;
     }
-    array[x][y] = (char) *set_num++;
+    array[x][y] = (char) (*set_num)++;
     return 0;
 
 }
-
-    
+int main (int argc, char** argv) {
+    char buf[20];
+    int m = 0;
+    int n = 0;
+    fgets(buf, 20, stdin);
+    printf("%s\n", buf);
+    m = strlen(buf) - 1;
+    int sets = 2;
+    int i = 0;
+    n = m;
+    char** array = (char**) calloc( n , sizeof(char*));
+    char* array_row;
+    while(strlen(buf) - 1 == m){
+        array_row = (char*) calloc(m,  sizeof(char));
+        strncpy(array_row, buf, n);
+        array[i++] = array_row;
+        fgets(buf, 20, stdin);
+   }
+   int j = 0;
+    for(i =0; i< n; i++) {
+        for(j =0; j<n; j++) {
+            check_neighbors(array, &sets, j, i,  n, n);
+        }
+    } 
+    printf("Number of disjoint blobs of zeroes %d\n", sets-2); 
+   
+}
