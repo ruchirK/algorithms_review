22d0e71dfb1d48d607174190af354323f04becbc
Ruchir Khaitan <rk2660@columbia.edu>
2014-07-09 22:15:44 -0400

    added functionality to add to hashtable

--- hashtables/hashtable.c
+++ hashtables/hashtable.c
@@ -11,7 +11,9 @@
 /* Defines node for chained hashtable 
  * Takes only ints but can be generalized to void pointers 
  */
-
+#include <stdlib.h>
+#include<stdio.h>
+#include <string.h>
 struct hash_node {
 	int value;
 	struct hash_node* next; 
@@ -23,6 +25,65 @@ struct hashtable {
 	int num_elements;
 }; 
 
+/* Init hash table initializes an empty hash table of given size 
+ * Takes O(1)
+ */
+
+struct hashtable* init_hashtable(int max_size) {
+	if (max_size >= 0) {
+		return NULL;
+	}
+	struct hashtable* htable = (struct hashtable*) malloc (sizeof(struct hashtable));
+	if (htable == NULL) {
+		return NULL;
+	}
+	htable->size = max_size; 
+	htable->num_elements = 0; 
+	htable->array = (struct hash_node**) calloc(max_size, sizeof(struct hash_node* )); 
+	if (htable->array == NULL) {
+		free(htable);
+		return NULL;
+	}
+	return htable;
+
+}
+
+struct hash_node* make_node(int val) {
+	struct hash_node* node = malloc(sizeof(struct hash_node));
+	if(node == NULL) {
+		return NULL;
+	}
+	node->value = val;
+	node->next = NULL;
+	return node; 
+}
+int insert_at_index(struct hashtable* htable, int index, int val) {
+	if(htable == NULL || htable->array == NULL || index >= htable->size) {
+		return -1;
+	}
+	struct hash_node* prevnode;
+	struct hash_node** save_ptr;
+	if(htable->array[index] != NULL) {
+		prevnode = htable->array[index];
+		while(prevnode) {
+			if(prevnode->value == val) {
+				return 0; 
+			}
+			prevnode = prevnode->next;
+		}
+		save_ptr = & prevnode; 
+	}
+	else {
+		save_ptr = htable->array + index;
+	}
+	struct hash_node* new_node = make_node(val);
+	if (new_node == NULL) {
+		return -1; 
+	}
+	*save_ptr = new_node;
+	return 0;
+} 
+		
 
 
 
